cafc2cea89f6ee3c3e20d7c141259697e59fcfd7
Zeeshan Khan <zeeskhan1990@gmail.com>
2019-09-12 01:23:01 +0530

    Before splitting reducer

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,51 +2,49 @@ import React, { Component } from 'react';
 import {connect} from "react-redux"
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions'
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
 
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Results</button>
+                <ul>
+                    {this.props.storedResults.map((result) => {
+                        return (<li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>
+                                    {result.value}
+                                </li>)
+                    })}
+                    
+                </ul>
             </div>
         );
     }
 }
 
 const mapStateToProps = state => {
+    console.log(state)
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: 'INCREMENT'})
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddCounter: () => dispatch({type: actionTypes.ADD, value:5}),
+        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value:5}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (resultId) => dispatch({type:actionTypes.DELETE_RESULT, resultId})
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,49 @@
+import * as actionTypes from './actions'
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (currentState=initialState, action) => {
-    if(action.type === "INCREMENT") {
+    //Should be handled with switch-case, I know!!!
+    if(action.type === actionTypes.INCREMENT) {
         return {
             ...currentState,
             counter: currentState.counter + 1
+        }
+    } else if(action.type === actionTypes.DECREMENT) {
+        return {
+            ...currentState,
+            counter: currentState.counter - 1
+        }
+    } else if(action.type === actionTypes.ADD) {
+        return {
+            ...currentState,
+            counter: currentState.counter + action.value
+        }
+    } else if(action.type === actionTypes.SUBTRACT) {
+        return {
+            ...currentState,
+            counter: currentState.counter - action.value
+        }
+    } else if(action.type === actionTypes.STORE_RESULT) {
+        const [...newResults] = currentState.results
+        newResults.push({id: Math.floor(Date.now() / 1000), value: currentState.counter})
+        return {
+            ...currentState,
+            results: newResults
+        }
+    } else if(action.type === actionTypes.DELETE_RESULT) {
+        console.log(action)
+        let [...clonedResults] = currentState.results
+        clonedResults = clonedResults.filter((temp) => {
+            return temp.id !== action.resultId
+        })
+        debugger
+        return {
+            ...currentState,
+            results: clonedResults
         }
     }
     return currentState
